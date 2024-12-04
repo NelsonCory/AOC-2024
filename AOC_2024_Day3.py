10d0559075c3212e039c9685e18c81be3300830a
Cory Nelson <34819470+NelsonCory@users.noreply.github.com>
2024-12-03 23:17:13 -0600

    Day 3 part 1

--- AOC_2024_Day3.py
+++ AOC_2024_Day3.py
@@ -13,7 +13,13 @@ def part_1():
     data = loadData()
     mulRegex = "(mul\(\d+,\d+\))"
     mulList = re.findall(mulRegex,data)
-    print(mulList)
+    #print(mulList)
+    multSum = 0
 
+    #identify numbers, then multiply together and add to multSum
+    for i in mulList:
+        temp = i[4:-1].split(",")
+        multSum += int(temp[0]) * int(temp[1])
+    print(multSum)
 
-part_1()
+part_1() # 153469856
